f8adadafd463187e6c9c48928fb8cb6b5d86b763
b9 <hackerb9@member.fsf.org>
2026-05-08 00:01:51 -0700

    Switch to using PEEK(21333), PEEK(31444)

--- adjunct/id.py
+++ adjunct/id.py
@@ -2,11 +2,16 @@
 from glob import glob as glob
 import sys
 
-# February 2026
-# Hackerb9 believes PEEK(1) and PEEK(21358) are best for uniquely
-# identifying the different ROMs of the Kyotronic Sisters.
+# May 2026
 
-# This program simply reads byte 1 and 21358 of each ROM file and prints them.
+# Hackerb9 believes PEEK(1) is good for generally identifying the
+# different Model-T models, but inadequate for international use.
+# Using the romcmp.py program, it seems that one can distinguish
+# models better using two peeks: PEEK(21333) and PEEK(31444).
+
+# This program simply reads bytes 1, 21333, and 31444 from each ROM
+# file and prints them.
+
 if len(sys.argv) == 1:
     romfiles = glob("ROMs/*.bin")
 else:
@@ -16,36 +21,29 @@ results=[]
 for f in romfiles:
     data=open(f, "rb").read()
     if len(data)<32767: continue
-    results.append(f"{data[1]:5d}{data[21358]:8d}     {f}")
+    results.append(f"{data[1]:5d}{data[21333]:8d}{data[31444]:8d}\t    {f}")
 
-print("PEEK(1)\t(21358)   ROM FILE")
+print("PEEK(1)\t(21333)\t(31444)\t    ROM FILE")
 print('\n'.join(sorted(results)))
 
-# PEEK(1) lumps together T102 regardless of country (US, UK, and Norway);
-#	  also lumps together PC8201A and PC8300.
-# PEEK(21358) lumps together US M100 and US T102, but not UK or NO.
-#
-# Both correctly lump together the y2k variation with the original ROM.
 
-# PEEK(1) (21358)   ROM FILE
-#    35      35     M10_System_ROM_EU.orig.bin
-#    35      35     M10_System_ROM_EU.y2k.bin
-#    51      83     TRS-80_Model_100.orig.bin
-#    51      83     TRS-80_Model_100.y2k.bin
-#    72     209     NEC_PC-8300_Beckman-E3.2.bin
-#   125     205     M10_System_ROM_NorthAmerica.orig.bin
-#   125     205     M10_System_ROM_NorthAmerica.y2k.bin
-#   144     254     TANDY_600_BASIC.bin
-#   148     101     NEC_PC-8201A.orig.bin
-#   148     101     NEC_PC-8201A.y2k.bin
-#   148     235     NEC_PC-8300.orig.bin
-#   148     235     NEC_PC-8300.y2k.bin
-#   167      83     TANDY_Model_102.us.orig.bin
-#   167      83     TANDY_Model_102.us.y2k.bin
-#   167      96     TANDY_Model_102.uk.orig.bin
-#   167      96     TANDY_Model_102.uk.y2k.bin
-#   167     123     Televerket-Modell100.orig.bin
-#   171       9     TANDY_Model_200.M15.orig.bin
-#   171       9     TANDY_Model_200.M15.y2k.bin
-#   225     194     KC-85.orig.bin
-#   225     194     KC-85.y2k.bin
+# PEEK(1)(21333)(31444)	    ROM FILE			VARIANTS ALLOWED
+#    35     213      33	    Olivetti_M10_Eur		y2k
+#    51       2      82	    TRS-80_Model_100 		y2k, REX+y2k
+#    51     202      82	    TRS-80_Model_100+LibROM-1.1a
+#   125      83     124	    Olivetti_M10_North_America	y2k
+#   148      83      37	    NEC_PC-8300			y2k, y2k_VirT
+#   148     244      37	    NEC_PC-8201A		y2k, REX_y2k, y2k_VirT
+#   148     244     126	    NEC_PC-8201+Japan
+
+#   167       2     127	    Tandy_102_us		y2k, REX_y2k
+#   167       2     127	    TRS-80_Model_100_uk+y2k.bin
+#   167       2     127	    TRS-80_Model_100_us+26-3802B.bin
+
+#   167     105     127	    Tandy_102_uk		y2k
+#   167     254     127	    Televerket_Modell_100+orig.bin
+#   171     205     244	    Tandy_200			y2k, REX, REX_y2k
+#   195     102      73	    Tandy_200_Multiplan
+
+#   225      26     255	    Kyocera_Kyotronic_85	y2kk
+#   225      26     255	    TRS-80_Model_100_uk+skl.bin
