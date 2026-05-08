7ffb1a9f168ef54bf48f9ea3ed9a4464014f4afc
b9 <hackerb9@member.fsf.org>
2026-05-07 23:37:01 -0700

    Documentation
    # It is well known that PEEK(1) can distinguish between the Model T's.
    # However, it cannot tell nationalized models apart. (Tandy 102
    # US/UK/NO; NEC PC-8201/PC-8201A/PC-8300).
    #
    # Is there any single byte address that would uniquely identify every
    # Kyotronic Sister? This program shows the answer is "No".
    
    # At least two peeks are needed to fully distinguish the ROMs hackerb9
    # has collected. PEEK(21333) and PEEK(31444) work.
    
    # 1. PEEK(1) is still quite useful for the most common Model-T
    #    computers and is still recommended.
    
    #    PEEK(1)
    #         35      Olivetti_M10_Eur
    #         51      TRS-80_Model_100
    #        125      Olivetti_M10_North_America
    #        148      NEC_PC-8201 (Japan) / NEC_PC-8201A / NEC_PC-8300
    #        167      Tandy_102_us / Tandy_102_uk / Televerket_Modell_100
    #        171      Tandy_200_us
    #        225      Kyocera_Kyotronic_85
    
    # 2. A second PEEK can distinguish the Tandy 102 US/UK/NO.
    
    #    I currently use PEEK(21333) even though it is not very useful on
    #    its own. It works here and is easier to remember. It also
    #    separates the PC-8201 and 8201A from the PC-8300.
    #
    #    PEEK(1)     (21333)
    #        148       244        NEC_PC-8201 (Japan) / NEC_PC-8201A
    #        148        83        NEC_PC-8300
    #        167       105        Tandy_102_uk
    #        167       254        Televerket_Modell_100
    #        167         2        Tandy_102_us
    
    # 3. A third PEEK can distinguish between the NEC PC-8201 and PC-8201A.
    #    I use 31444.
    #
    #    PEEK(1) (21333) (31444)
    #        148     83      37        NEC_PC-8300
    #        148    244      37        NEC_PC-8201+A
    #        148    244     126        NEC_PC-8201+Japan
    
    #  * Note that the previous two PEEKs actually provide a unique ID for
    #    every model of ROM hackerb9 has found, as of May 2026.
    #
    #    PEEK(21333) (31444)
    #            2      82        TRS-80_Model_100
    #            2     127        Tandy_102_us
    #           26     255        Kyocera_Kyotronic_85
    #           83      37        NEC_PC-8300
    #           83     124        Olivetti_M10_North_America
    #          105     127        Tandy_102_uk
    #          205     244        Tandy_200
    #          213      33        Olivetti_M10_Eur
    #          244      37        NEC_PC-8201+A
    #          244     126        NEC_PC-8201+Japan
    #          254     127        Televerket_Modell_100
    
    # 4. Is there a single best PEEK? Yes and no.
    #
    #    For the ROMs I currently have, only a handful of addresses have
    #    the maximum discerning power on their own: 21351, 21356..8.
    #    However, I cannot recommend any of them as they can not
    #    distinguish two of the most common Model-Ts: the TRS-80 Model 100
    #    and the Tandy 102.
    
    #    PEEK(21358)
    #           9        Tandy_200+M15
    #          35        Olivetti_M10_Eur
    #          83        Tandy_102_us / TRS-80_Model_100
    #          96        Tandy_102_uk
    #         101        NEC_PC-8201+A  / NEC_PC-8201+Japan
    #         123        Televerket_Modell_100
    #         194        Kyocera_Kyotronic_85
    #         205        Olivetti_M10_North_America
    #         235        NEC_PC-8300

--- adjunct/romcmp.py
+++ adjunct/romcmp.py
@@ -1,51 +1,21 @@
 #!/usr/bin/python
 
-# It is well known that PEEK(1) can distinguish between the Model T's,
-# but it confuses PC8201 and 8300.
-# Address: 1
-#          pc8201 148
-#          pc8300 148
-#          kc85 225
-#          m10 35
-#          m100 51
-#          m102 167
-#          t200 171
+# It is well known that PEEK(1) can distinguish between the Model T's.
+# However, it cannot tell nationalized models apart. (Tandy 102
+# US/UK/NO; NEC PC-8201/PC-8201A/PC-8300).
 #
-# b9 wonders if there is any single byte address that would uniquely
-# identify every Kyotronic Sister. The answer is No, and especially NO
-# if international ROMs need to be distinguished from US.
+# Is there any single byte address that would uniquely identify every
+# Kyotronic Sister? This program shows the answer is "No".
 
-# 1. PEEK(1) may not be complete, but it is still useful and I
-#    (hackerb9) recommend it for general use. 
-
-# 2. A second PEEK can distinguish the Tandy 102 US from UK and NO.
-#    Only a handful of addresses work: 21333, 21356, 21357, & 21358.
-#    I use PEEK(21358). It also separates 8201 from 8300.
-#    PEEK(1) (21358) [ == 536Eh ]
-#        167     96  Tandy_102_uk+orig
-#        167     83  Tandy_102_us+orig
-#        167    123  Televerket_Modell_100+orig
-#        148    101  NEC_PC-8201+A_orig
-#        148    101  NEC_PC-8201+Japan_orig
-#        148    235  NEC_PC-8300+orig
-     
-# 3. A third PEEK can distinguish between the NEC PC-8201 and PC-8201A.
-#    Many addresses would work. 31456 is an easy one to remember.
-#    PEEK(1) (21358) (31456)                
-#       148     101     124          PC-8201 
-#       148     101      32          PC-8201A
-#       148     235      32          PC-8300 
-
-# Note that, as of May 2026,  PEEK(1) is redundant if one is using the latter two peeks
-# in
+# At least two peeks are needed to fully distinguish the ROMs hackerb9
+# has collected. PEEK(21333) and PEEK(31444) work. See notes below.
 
 from glob import glob as glob
 import sys
 
 # Addresses which are already being used to disambiguate. Can be empty.
-# already_given = [ 1 ]
-#already_given = [ 21358 ]
-already_given = [ 21333 ]
+#already_given = []
+already_given = [ 31444 ]
 
 rom = {}
 
@@ -76,7 +46,6 @@ if best <= 1: raise ValueError
  
 for i in range(32768):
     if len(seen[i]) >= best:
-
 #        if rom['NEC_PC-8201+A_orig'][i] == rom['NEC_PC-8201+Japan_orig'][i]: continue
 #        if rom['Tandy_102_uk+orig'][i] == rom['Tandy_102_us+orig'][i]: continue
 
@@ -90,3 +59,72 @@ for i in range(32768):
                 print (f"{rom[name][x]:8d}", end="")
             print(f'{rom[name][i]:8d}\t{name}')
 
+
+
+# 1. PEEK(1) is still quite useful for the most common Model-T
+#    computers and is still recommended.
+
+#    PEEK(1) 
+#         35      Olivetti_M10_Eur
+#         51      TRS-80_Model_100
+#        125      Olivetti_M10_North_America
+#        148      NEC_PC-8201 (Japan) / NEC_PC-8201A / NEC_PC-8300
+#        167      Tandy_102_us / Tandy_102_uk / Televerket_Modell_100
+#        171      Tandy_200_us
+#        225      Kyocera_Kyotronic_85
+
+# 2. A second PEEK can distinguish the Tandy 102 US/UK/NO.
+
+#    I currently use PEEK(21333) even though it is not very useful on
+#    its own. It works here and is easier to remember. It also
+#    separates the PC-8201 and 8201A from the PC-8300.
+#
+#    PEEK(1)     (21333)
+#        148       244        NEC_PC-8201 (Japan) / NEC_PC-8201A
+#        148        83        NEC_PC-8300
+#        167       105        Tandy_102_uk
+#        167       254        Televerket_Modell_100
+#        167         2        Tandy_102_us
+
+# 3. A third PEEK can distinguish between the NEC PC-8201 and PC-8201A.
+#    I use 31444.
+#
+#    PEEK(1) (21333) (31444)                
+#        148     83      37        NEC_PC-8300
+#        148    244      37        NEC_PC-8201+A
+#        148    244     126        NEC_PC-8201+Japan
+
+#  * Note that the previous two PEEKs actually provide a unique ID for
+#    every model of ROM hackerb9 has found, as of May 2026. 
+#
+#    PEEK(21333) (31444)
+#            2      82        TRS-80_Model_100
+#            2     127        Tandy_102_us
+#           26     255        Kyocera_Kyotronic_85
+#           83      37        NEC_PC-8300
+#           83     124        Olivetti_M10_North_America
+#          105     127        Tandy_102_uk
+#          205     244        Tandy_200
+#          213      33        Olivetti_M10_Eur
+#          244      37        NEC_PC-8201+A
+#          244     126        NEC_PC-8201+Japan
+#          254     127        Televerket_Modell_100
+
+# 4. Is there a single best PEEK? Yes and no. 
+#
+#    For the ROMs I currently have, only a handful of addresses have
+#    the maximum discerning power on their own: 21351, 21356..8.
+#    However, I cannot recommend any of them as they can not
+#    distinguish two of the most common Model-Ts: the TRS-80 Model 100
+#    and the Tandy 102.
+
+#    PEEK(21358)
+#           9        Tandy_200+M15
+#          35        Olivetti_M10_Eur
+#          83        Tandy_102_us / TRS-80_Model_100
+#          96        Tandy_102_uk
+#         101        NEC_PC-8201+A  / NEC_PC-8201+Japan
+#         123        Televerket_Modell_100
+#         194        Kyocera_Kyotronic_85
+#         205        Olivetti_M10_North_America
+#         235        NEC_PC-8300
