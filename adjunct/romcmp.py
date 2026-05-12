48b2cabcb23fb4046c79034f5d33421437b32d07
b9 <hackerb9@member.fsf.org>
2026-05-12 00:40:02 -0700

    Group roms by model (before the +)

--- adjunct/romcmp.py
+++ adjunct/romcmp.py
@@ -4,22 +4,58 @@
 # However, it cannot tell nationalized models apart. (Tandy 102
 # US/UK/NO; NEC PC-8201/PC-8201A/PC-8300).
 #
-# Is there any single byte address that would uniquely identify every
+# Is there any single byte address that would uniquely identify every.
 # Kyotronic Sister? This program shows the answer is "No".
 
-# At least two peeks are needed to fully distinguish the ROMs hackerb9
-# has collected. PEEK(21333) and PEEK(31444) work. See notes below.
+# At least two, maybe three peeks are needed to fully distinguish the
+# set of ROMs hackerb9 has collected (so far).
+# PEEK(21333) and PEEK(31444) are memorable and mostly work.
+
+# Grouping ROMs by date order (MMDDYY, DDMMYY, YYMMDD), shows that (so
+# far) a single PEEK would suffice. For example:
+
+# |   PEEK(6468)	| FORMAT |
+# |---------------------|--------|
+# | 	5		| YYMMDD |
+# | 	25, 74, 110	| DDMMYY |
+# | 	154, 200	| MMDDYY |
+
+    # PEEK   (6468)
+    #            5    YYMMDD+8201
+    #            5    YYMMDD+8201A
+    #            5    YYMMDD+8201A_y2k
+    #            5    YYMMDD+8300
+    #            5    YYMMDD+8300_y2k
+    #           25    DDMMYY+m100_no
+    #           25    DDMMYY+m100_uk
+    #           25    DDMMYY+m100_uk_y2k
+    #           25    DDMMYY+t102_uk
+    #           25    DDMMYY+t102_uk_y2k
+    #           74    DDMMYY+librom
+    #          110    DDMMYY+k85
+    #          110    DDMMYY+k85_y2k
+    #          110    DDMMYY+m10eu
+    #          110    DDMMYY+m10eu_y2k
+    #          154    MMDDYY+m100
+    #          154    MMDDYY+m100_y2k
+    #          154    MMDDYY+t102
+    #          154    MMDDYY+t102_y2k
+    #          200    MMDDYY+t200
+    #          200    MMDDYY+t200_y2k
+
 
 from glob import glob as glob
 from collections import defaultdict
-import sys
+import os, sys
+
 
 # Addresses which are already being used to disambiguate. Can be empty.
 already_given = [6451]
 #already_given = [ 21358, 31439 ]
 #already_given = [ 21333, 31444, 6451 ]
 #already_given = [ 1, 31444 ]
-#already_given = [ 31444, 6451 ]
+already_given = [ 31444, 6451 ]
+already_given = [  ]
 
 rom = {}
 
@@ -33,63 +69,79 @@ files = sorted(files)
 
 model=defaultdict(dict)         # "Model" is part before '+' ("Tandy_102_uk")
 				# "Variant" is part after '+' ("y2k")
+namemv=defaultdict(dict)
+
 removed=defaultdict(dict)
 for f in files:
+    if os.path.isdir(f):
+        continue
     name=f.rstrip(".bin")
-
-    if name.find('+') != -1:
-        (m, v) = name.split('+')
-    else:
-        m = name
-        v = ""
-    model[m][v]=name
-        
     rom[name] = open(f, "rb").read(32768)
-    if len(rom[name])<32768: del rom[name]
+    if len(rom[name]) == 32768:
+        if name.find('+') != -1:
+            (m, v) = name.split('+')
+        else:
+            m = name
+            v = ""
+        if m.rfind('/') != -1:
+            m = m[m.rfind('/')+1:]
+        model[m][v]=name
+        namemv[name]=(m,v)
+    else:
+        del rom[name]
 
 
 # Ignore addresses where variants differ.
-blocklist = []                  
-for m in model:
-    if len(model[m]) == 1:
-        continue
-    print(f"Merging variants of {m}: {list(model[m].keys())}")
-    for i in range(0, 32768):
-        seen = set()
-        for v,name in model[m].items():
-            seen.add( rom[name][i] )
-            if len(seen) > 1:
-                blocklist.append(i)
-    # Keep only one of the ROM variants.
-    x = list(model[m].keys())[0]
-    rom[m] = rom[model[m][x]]
-    for v in model[m]:
-        removed[model[m][v]] = rom[model[m][v]]
-        del rom[model[m][v]]
+modelrom = defaultdict(dict)
 
+for m in model:
+    print(f"Merging variants of model {m}: {list(model[m].keys())}")
+    modelrom[m] = defaultdict(dict)
+    for i in range(0, 32768):
+        for v,name in model[m].items():
+#            for value in modelrom[m][i]:# Presume PEEK(x) is also being used
+#                for x in already_given: 
+#                    value = value<<8 + modelrom[m][x]
+ #               seen[i].add( value )
+            if i in modelrom[m]:
+                modelrom[m][i].append( rom[name][i] )
+            else:
+                modelrom[m][i] = [ rom[name][i] ]
+
+    # Keep only one of the ROM variants.
+#    x = list(model[m].keys())[0]
+#    rom[m] = rom[model[m][x]]
+
+#    for v in model[m]:
+#        removed[model[m][v]] = rom[model[m][v]]
+#        del rom[model[m][v]]
+
+print("")
 # Main routine
 
+# For every address, check each model's set of values at that address
+# and make sure there is no overlap (intersection) with the other models..
+count = {}
 seen = {}
 for i in range(0, 32768):
     seen[i] = set()
-    if i in blocklist: continue
-    for name in rom:
-        # Count unique values for PEEK(i) for each ROM. 
-        value = rom[name][i]
-        for x in already_given: 	# Presume PEEK(x) is also being used
-            value = value<<8 + rom[name][x]
-        seen[i].add( value )
+    count[i] = 0
+    for m in modelrom:
+        # Count unique values for PEEK(i) for each model of ROM. 
+        if seen[i].isdisjoint( modelrom[m][i] ):
+            count[i]=count[i]+1
+        seen[i].update( modelrom[m][i] )
        
-best = max([ len(seen[s]) for s in seen ])
-if best < len(rom):
-    print( f'At best can distinguish {best} of the {len(rom)} ROMs' )
+best = max( count.items(), key = lambda x: x[1] )[1]
+if best < len(model):
+    print( f'At best can distinguish {best} of the {len(model)} models ({len(rom)} ROMs)' )
 else:
-    print( f'Can distinguish all of the {len(rom)} ROMs' )
+    print( f'Can distinguish all of the {len(model)} models ({len(rom)} ROMs)' )
 
 if best <= 1: raise ValueError
  
 for i in range(32768):
-    if len(seen[i]) >= best:
+    if count[i] >= best:
 #        if rom['NEC_PC-8201+A_orig'][i] == rom['NEC_PC-8201+Japan_orig'][i]: continue
 #        if rom['Tandy_102_uk+orig'][i] == rom['Tandy_102_us+orig'][i]: continue
 
